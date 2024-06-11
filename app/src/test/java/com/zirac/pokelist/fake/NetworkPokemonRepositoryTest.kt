6de7c0ccc613fdc8826b761d625bff8ec6601cdd
Zira Cook <26914409+ziracook@users.noreply.github.com>
2024-06-10 21:06:53 -0700

    refactored test

--- app/src/test/java/com/zirac/pokelist/fake/NetworkPokemonRepositoryTest.kt
+++ app/src/test/java/com/zirac/pokelist/fake/NetworkPokemonRepositoryTest.kt
@@ -3,30 +3,30 @@ package com.zirac.pokelist.fake
 import com.zirac.pokelist.data.NetworkPokemonRepository
 import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.test.runTest
+import org.junit.Before
 import org.junit.Test
 
 class NetworkPokemonPhotosRepository {
-    @Test
-    fun getPokemonList_verifyPokemonList() = runTest {
-        val repository = NetworkPokemonRepository(
+    private lateinit var repository: NetworkPokemonRepository
+    @Before
+    fun setup() {
+        repository = NetworkPokemonRepository(
             pokemonApiService = FakePokemonApiService()
         )
+    }
+
+    @Test
+    fun getPokemonList_verifyPokemonList() = runTest {
         assertEquals(FakeDataSource.pokemonListResponse, repository.getPokemonList())
     }
 
     @Test
     fun getPokemonByName_verifyPokemon() = runTest {
-        val repository = NetworkPokemonRepository(
-            pokemonApiService = FakePokemonApiService()
-        )
         assertEquals(FakeDataSource.pokemonResponse, repository.getPokemonByName(""))
     }
 
     @Test
     fun getNextPokemonPage_verifyPokemonList() = runTest {
-        val repository = NetworkPokemonRepository(
-            pokemonApiService = FakePokemonApiService()
-        )
         assertEquals(FakeDataSource.pokemonListResponse, repository.getNextPokemonPage(""))
     }
 }
